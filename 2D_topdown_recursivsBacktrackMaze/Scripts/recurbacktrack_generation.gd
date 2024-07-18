67a1e8231fe4f626f66f5129dcb5d34ab7d820ea
SenpaiJ <jason-zi-xin.yiu@students.mq.edu.au>
2024-07-18 17:43:48 +1000

    update
    backtrack maze implemented

--- 2D_topdown_recursivsBacktrackMaze/Scripts/recurbacktrack_generation.gd
+++ 2D_topdown_recursivsBacktrackMaze/Scripts/recurbacktrack_generation.gd
@@ -8,18 +8,24 @@ const W = 8
 
 var wall_directions:Dictionary = {Vector2.UP: N, Vector2.RIGHT: E, Vector2.DOWN: S, Vector2.LEFT: W}
 var tile_size: int = 16
-var width: int = 9
-var height: int = 5
+var width: int = 18
+var height: int = 10
 var unvisited: Array
+
+#Tileset properties
+var tileset_collumns: int = 4
+var tileset_rows:     int = 4
+
 @onready var tilemap:TileMap = $TileMap
 
 func _ready():
 	randomize()
 	create_maze()
-	var test_cell: Array = get_univisted_neighbours(Vector2(0,0), unvisited)
-	print(test_cell)
 
-
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		randomize()
+		create_maze()
 
 func get_univisted_neighbours(cell: Vector2, univisited: Array) -> Array:
 	#Returns an array of the cells univisited neighbours
@@ -36,10 +42,41 @@ func create_maze():
 	tilemap.clear()
 	for x in range(width):
 		for y in range(height):
+			#register tiles
 			var cell: Vector2 = Vector2(x,y)
 			unvisited.append(cell)
-			tilemap.set_cell(0, cell, 0, Vector2(0,0))
+			tilemap.set_cell(0, cell, 0, int_to_atlas(N|E|S|W))
+			
+			#print(atlas_to_int(tilemap.get_cell_atlas_coords(0, cell)) - wall_directions[Vector2.UP])
+	
+	var current_cell : Vector2 = Vector2.ZERO
+	unvisited.erase(current_cell)
+	
+	while  !unvisited.is_empty():
+		#iterate backtrack algo
+		var neighbours: Array = get_univisted_neighbours(current_cell, unvisited)
+		if neighbours:
+			var next_cell: Vector2 = neighbours.pick_random()
+			#knock down walls between two cells
+			remove_walls(current_cell, next_cell)
+			stack.append(current_cell)
+			current_cell = next_cell
+			unvisited.erase(current_cell)
+			await get_tree().create_timer(0.01).timeout
+		else:
+			current_cell = stack.pop_back()
 
 
-func id_to_atlas():
-	pass
+func remove_walls(a_cell: Vector2, b_cell: Vector2):
+	var a_to_b: Vector2i = int_to_atlas(wall_directions[b_cell - a_cell])
+	var b_to_a: Vector2i = int_to_atlas(wall_directions[a_cell - b_cell])
+	tilemap.set_cell(0, a_cell, 0, tilemap.get_cell_atlas_coords(0, a_cell) - a_to_b)
+	tilemap.set_cell(0, b_cell, 0, tilemap.get_cell_atlas_coords(0, b_cell) - b_to_a)
+
+func int_to_atlas(num: int) -> Vector2:
+	var atlas_coord: Vector2 = Vector2(num % tileset_collumns, num / tileset_rows)
+	return atlas_coord
+
+func atlas_to_int(coord: Vector2) -> int:
+	var num = coord.x + (coord.y * tileset_rows)
+	return num
